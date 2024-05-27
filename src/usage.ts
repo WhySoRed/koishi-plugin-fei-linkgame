af991b15bfdbcff4d8bee1fe2f144e4ffd003327
WhySoRed <lycorismaybe@outlook.com>
2024-05-27 14:29:46 +0800

    modified:   src/usage.ts

--- src/usage.ts
+++ src/usage.ts
@@ -1,3 +1,4 @@
+import { Random } from "koishi";
 import { Config } from "./config";
 
 export let usage = `
@@ -51,16 +52,33 @@ export let usage = `
 <div class="cell" style="grid-area: 5 / 5;">
   <div id="block15" class="block" style="background-position: -135px -135px; background-size: 400% 400%;"></div>
 </div>
+<div class="cell" style="grid-area: 2 / 6;">
+<div id="block16" class="block" style="background: #fcf5f7;"></div>
+</div>
+<div class="cell" style="grid-area: 3 / 6;">
+<div id="block17" class="block" style="background: #fcf5f7;">⛓️</div>
+</div>
+<div class="cell" style="grid-area: 4 / 6;">
+<div id="block18" class="block" style="background: #fcf5f7;">⛓️</div>
+</div>
+<div class="cell" style="grid-area: 5 / 6;">
+<div id="block19" class="block" style="background: #fcf5f7;">👀</div>
+</div>
+<div class="timecell">
+<div class="timelimit">
+  <div class="timeleft"></div>
+</div>
+</div>
 </div>
 
 <style>
 #linkGameLogo {
   position: relative;
-  width: 260px;
-  height: 260px;
+  width: 310px;
+  height: 300px;
   display: grid;
-  grid-template-columns: 0.6fr repeat(4, 1fr) 0.6fr;
-  grid-template-rows: 0.6fr repeat(4, 1fr) 0.6fr;
+  grid-template-columns: 0.6fr repeat(5, 1fr) 0.6fr;
+  grid-template-rows: 0.6fr repeat(4, 1fr) 0.4fr 1fr;
   background: linear-gradient(to bottom right, #002a33, #002129);
 }
 
@@ -86,13 +104,53 @@ export let usage = `
 }
 
 #linkGameLogo .block {
+  display: flex;
+  justify-content: center;
+  align-items: center;
   width: 80%;
   height: 80%;
+  border-radius: 10%;
+  font-size: 20px;
   border-radius: 10%;
   box-shadow: #00a5bf 3px 3px;
   background: #fcf5f7;
   background-image: url("https://koishi.chat/logo.png");
   background-repeat: no-repeat;
+}
+
+#linkGameLogo .timecell {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  grid-row: 7;
+  grid-column: 2/7;
+}
+
+#linkGameLogo .timelimit {
+  background-color: #de3163;
+  width: 230px;
+  height: 20%;
+}
+
+#linkGameLogo .timeleft {
+  background: linear-gradient(to left, green, red);
+  background-clip: content-box;
+  box-sizing: border-box;
+  width: 100%;
+  height: 100%;
+  animation: time 10s infinite;
+}
+
+@keyframes time {
+  0% {
+    padding-right: -30%;
+  }
+  80% {
+    padding-right: 100%;
+  }
+  100% {
+    padding-right: -30%;
+  }
 }
 </style>
 
@@ -102,18 +160,16 @@ export let usage = `
 小心不要沉迷哦...
 `;
 
-
-export async function updateUsage( config :Config) {
-  
+export async function updateUsage(config: Config) {
   const style = `
 <style>
 #linkGameLogo {
   position: relative;
-  width: 260px;
-  height: 260px;
+  width: 310px;
+  height: 300px;
   display: grid;
-  grid-template-columns: 0.6fr repeat(4, 1fr) 0.6fr;
-  grid-template-rows: 0.6fr repeat(4, 1fr) 0.6fr;
+  grid-template-columns: 0.6fr repeat(5, 1fr) 0.6fr;
+  grid-template-rows: 0.6fr repeat(4, 1fr) 0.4fr 1fr;
   background: linear-gradient(to bottom right, ${config.backGroundColorStart}, ${config.backGroundColorEnd});
 }
 
@@ -139,6 +195,9 @@ export async function updateUsage( config :Config) {
 }
 
 #linkGameLogo .block {
+  display: flex;
+  justify-content: center;
+  align-items: center;
   width: 80%;
   height: 80%;
   border-radius: 10%;
@@ -147,8 +206,72 @@ export async function updateUsage( config :Config) {
   background-image: url("https://koishi.chat/logo.png");
   background-repeat: no-repeat;
 }
+
+.timecell {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  grid-row: 7;
+  grid-column: 2/7;
+}
+
+.timelimit {
+  background-color: ${config.lineColor};
+  width: 230px;
+  height: 20%;
+}
+
+.timeleft {
+  background: linear-gradient(to left, ${config.timeStartColor}, ${config.timeEndColor});
+  background-clip: content-box;
+  box-sizing: border-box;
+  width: 100%;
+  height: 100%;
+  animation: time 10s infinite;
+}
+
+@keyframes time {
+  0% {
+    padding-right: -30%;
+  }
+  80% {
+    padding-right: 100%;
+  }
+  100% {
+    padding-right: -30%;
+  }
+}
 </style>  
-`
-  const styleRegExp = /<style[^>]*>([\s\S]*?)<\/style>/g
+`;
+  const styleRegExp = /<style[^>]*>([\s\S]*?)<\/style>/g;
   usage = usage.replace(styleRegExp, style);
+
+  if (config.pattermType.length) {
+    let randomPattern: string[];
+    if (config.pattermType.length >= 4) {
+      randomPattern = Random.shuffle(config.pattermType).slice(0, 4);
+    }
+    else {
+      randomPattern = config.pattermType;
+      while (randomPattern.length < 4) {
+        randomPattern.push(Random.pick(config.pattermType));
+      }
+    }
+    usage = usage.replace(
+      /<div id="block16".*<\/div>/,
+      `<div id="block16" class="block" style="background: ${config.blockColor};">${randomPattern[0]}</div>`
+    );
+    usage = usage.replace(
+      /<div id="block17".*<\/div>/,
+      `<div id="block17" class="block" style="background: ${config.blockColor};">${randomPattern[1]}</div>`
+    );
+    usage = usage.replace(
+      /<div id="block18".*<\/div>/,
+      `<div id="block18" class="block" style="background: ${config.blockColor};">${randomPattern[2]}</div>`
+    );
+    usage = usage.replace(
+      /<div id="block19".*<\/div>/,
+      `<div id="block19" class="block" style="background: ${config.blockColor};">${randomPattern[3]}</div>`
+    );
+  }
 }
